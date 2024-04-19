9ff8514f83a83896021d8bfbaac96a7c1670984c
Andre Felipe <andrefelipe.pi2@gmail.com>
2024-04-18 21:11:16 -0300

    Mensagem sobre o IMC

--- App.js
+++ App.js
@@ -28,8 +28,11 @@ export default function App() {
     setResultado(imc.toFixed(1))
 
     if(imc < 18.5) {
-      alert('Você está abaixo do peso. Procure um especialista')
-      return
+      return setResultado(imc.toFixed(1) + ' \nVocê está abaixo do peso. Procure um especialista na área.')
+    } else if(imc >= 18.6 && imc <= 24.9) {
+      return setResultado(imc.toFixed(1) + ' \nParabéns! Seu peso está normal.')
+    } else if(imc >= 25) {
+      return setResultado(imc.toFixed(1) + ' \nVocê está acima do peso! Procure um especialista na área.')
     }
   }
   
